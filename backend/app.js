99aa4d00431b85484b07e97b0a6aeb4503122b0e
Saairaam Prasad <saairaamprasad@gmail.com>
2026-04-18 15:59:09 +0530

    Add Netlify domain to CORS whitelist

--- backend/app.js
+++ backend/app.js
@@ -16,6 +16,7 @@ const allowedOrigins = [
 	"http://localhost:5176",
 	"http://127.0.0.1:5173",
 	"http://127.0.0.1:5174",
+	"https://amrutham-management.netlify.app",
 	process.env.FRONTEND_URL,
 ].filter(Boolean);
 
